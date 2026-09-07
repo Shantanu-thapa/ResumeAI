cdf59ff38af5c6f313558168ab891c28b64288c2
Shantanu-thapa <shantanuthapa021@gmail.com>
2026-09-07 11:14:28 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API = "https://resumeai-lyzc.onrender.com/";
+const API = import.meta.env.VITE_API_URL;
 
 function App() {
     const [token, setToken] = useState(localStorage.getItem("token"));
